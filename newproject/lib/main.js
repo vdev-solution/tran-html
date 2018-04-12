a03bba2611aef0712a793c54b280bd3c1e225d65
Tran Ly Tan <tantran.lee@gmail.com>
2018-04-12 17:53:43 +0700

    wdw

--- newproject/lib/main.js
+++ newproject/lib/main.js
@@ -1,3 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<h1>zzz</h1>,document.getElementById('root'));
+$(document).ready(function(){
+  console.log("zz");
+  $('.menu-bar-title-01').click(function(){
+    console.log("click");
+    $('.menu-bar-title-01').toggleClass('red');
+    $('.menu-bar-ul-01').toggle();
+  });
+  $('.menu-bar-title-02').click(function(){
+    console.log("click");
+    $('.menu-bar-title-02').toggleClass('red');
+    $('.menu-bar-ul-02').toggle();
+  });
+  $('.bxh-icon').click(function(event){
+    console.log('icon click');
+    $(event.currentTarget).closest('.bxh-full').toggleClass('red');
+  });
+});
