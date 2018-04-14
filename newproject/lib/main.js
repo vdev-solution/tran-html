da347fe2eef835f02f88b4b2b09606d099c885f1
Tran Ly Tan <tantran.lee@gmail.com>
2018-04-14 19:21:59 +0700

    regr

--- newproject/lib/main.js
+++ newproject/lib/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  console.log("zz");
   $('.menu-bar-title-01').click(function(){
     console.log("click");
     $('.menu-bar-title-01').toggleClass('red');
@@ -11,53 +10,97 @@ $(document).ready(function(){
     $('.menu-bar-ul-02').toggle();
   });
   $('.bxh-icon').on('click',(event)=>{
-    $('.bxh-full').not($(event.currentTarget).closest('.match-info-thead').next().find('.bxh-full')).addClass('d-none');
-    $(event.currentTarget).closest('.match-info-thead').next().find('.bxh-full').toggleClass('d-none');
-    $(event.currentTarget).closest('.match-info-detail').children('.main-tbody').find('.icon-main').children().addClass('d-none')
+
+
+    $(event.currentTarget).closest('.match-info-detail').find('.bxh-icon').toggleClass('dark-red');
+    $('.full-icon-mb-tbody').children().removeClass('dark-red');
+    $('.all-icon-tbody').children().removeClass('dark-red');
+    $(event.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.bxh-full').toggleClass('d-none');
+    $('.bxh-full').not($(event.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.bxh-full')).addClass('d-none');
+    $('.icon-main').children().addClass('d-none');
+
   });
-  $('.btn-hide').on('click',(event)=>{
-    $(event.currentTarget).parent().toggleClass('d-none');
-    $('.mb-icon-bxh').removeClass('dark-red');
-    $('.ico-thead-full').removeClass('dark-red');
+
+  $('.match-info-thead').on('click',(e)=>{
+    console.log('thead click');
+    $('.icon-mb-tbody').addClass('d-none');
+    $(e.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.mb-icon').toggleClass('d-none');
+    $('.mb-icon').not($(e.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.mb-icon')).addClass('d-none');
+    $('.bxh-full').not($(e.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.bxh-full')).addClass('d-none');
   });
-  $('.bxh-icon-tbody').on('click',(event)=>{
+
+  $('.btn-hide').on('click',(e)=>{
+    $(e.currentTarget).parent().addClass('d-none');
+    $('.full-icon-mb-tbody').children().removeClass('dark-red');
+    $('.all-icon-tbody').children().removeClass('dark-red');
+    $('.mb-icon').children().removeClass('dark-red');
+    $('.bxh-icon').removeClass('dark-red');
+  });
+
+  $('.bxh-icon-tbody').on('click',(e)=>{
+    $('.all-icon-tbody').children().not($(e.currentTarget).closest('.main-tbody').find('.bxh-icon-tbody')).removeClass('dark-red');
+    $(e.currentTarget).closest('.main-tbody').find('.bxh-icon-tbody').toggleClass('dark-red');
+
+    $('.bxh-icon').removeClass('dark-red');
+    $('.icon-main').children().not($(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01')).addClass('d-none');
+    $(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01').toggleClass('d-none');
     $('.bxh-full').addClass('d-none');
-    $('.icon-01').not($(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01')).addClass('d-none');
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01').toggleClass('d-none');
-
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01').siblings().addClass('d-none');
-    $(event.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.bxh-full').addClass('d-none');
   });
-  $('.ds-icon-tbody').on('click',(event)=>{
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-02').toggleClass('d-none');
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-02').siblings().addClass('d-none');
-    $(event.currentTarget).closest('.match-info-detail').children('.bxh-full-thead').find('.bxh-full').addClass('d-none');
 
+
+  $('.ds-icon-tbody').on('click',(e)=>{
+    $(e.currentTarget).closest('.main-tbody').find('.ds-icon-tbody').toggleClass('dark-red');
+
+    $('.bxh-icon').removeClass('dark-red');
+
+    $('.icon-main').children().not($(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-02')).addClass('d-none');
+    $(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-02').toggleClass('d-none');
+    $('.bxh-full').addClass('d-none');
   });
-  $('.match-info-thead').on('click',(event)=>{
 
-    $(event.currentTarget).next().find('.mb-icon').toggleClass('d-none');
+
+  $('.tk-icon-tbody').on('click',(e)=>{
+    $(e.currentTarget).closest('.main-tbody').find('.tk-icon-tbody').toggleClass('dark-red');
+
+    $('.bxh-icon').removeClass('dark-red');
+
+
+
+    $('.icon-main').children().not($(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-03')).addClass('d-none');
+    $(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-03').toggleClass('d-none');
+    $('.bxh-full').addClass('d-none');
   });
-  $('.mb-icon-bxh').on('click',(event)=>{
-    $(event.currentTarget).parent().next().toggleClass('d-none');
-    $(event.currentTarget).toggleClass('dark-red');
+
+
+  $('.tt-icon-tbody').on('click',(e)=>{
+    $(e.currentTarget).closest('.main-tbody').find('.tt-icon-tbody').toggleClass('dark-red');
+
+    $('.bxh-icon').removeClass('dark-red');
+
+
+
+    $('.icon-main').children().not($(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-04')).addClass('d-none');
+    $(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-04').toggleClass('d-none');
+    $('.bxh-full').addClass('d-none');
   });
-  $('.main-title-name').on('click',(event)=>{
 
-    $(event.currentTarget).parent().children('.icon-mb-tbody').toggleClass('d-none');
-    $(event.currentTarget).toggleClass('red-mb');
 
+  $('.ct-icon-tbody').on('click',(e)=>{
+    $(e.currentTarget).closest('.main-tbody').find('.ct-icon-tbody').toggleClass('dark-red');
+
+    $('.bxh-icon').removeClass('dark-red');
+
+
+
+    $('.icon-main').children().not($(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-05')).addClass('d-none');
+    $(e.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-05').toggleClass('d-none');
+    $('.bxh-full').addClass('d-none');
+  });
+
+  $('.main-title-name').on('click',(e)=>{
+    $(e.currentTarget).closest('.main-tbody').children('.icon-mb-tbody').toggleClass('d-none');
+    $('.icon-mb-tbody').not($(e.currentTarget).closest('.main-tbody').children('.icon-mb-tbody')).addClass('d-none');
+    $('.mb-icon').addClass('d-none');
   })
-  $('.mb-icon-bxh-tbody').on('click',(event)=>{
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01').toggleClass('d-none');
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-01').siblings().addClass('d-none');
-    $(event.currentTarget).siblings().removeClass('dark-red');
-    $(event.currentTarget).toggleClass('dark-red');
-  });
-  $('.mb-icon-ds-tbody').on('click',(event)=>{
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-02').toggleClass('d-none');
-    $(event.currentTarget).closest('.main-tbody').children('.icon-full-tbody').find('.icon-02').siblings().addClass('d-none');
-    $(event.currentTarget).siblings().removeClass('dark-red');
-    $(event.currentTarget).toggleClass('dark-red');
-  });
+
 });
